ef0aa51ed98b29d2b88f1ae7e78e4f37b11f79ad
Daniel Zumwalt <dazumwal@microsoft.com>
2019-09-10 20:32:02 -0700

    add publish script, postcss with autoprefixer

---
+++ danzumwalt-com/publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $0
+
+cd "$DIR/.."
+pwd
+rm -rf gh-pages
+mkdir gh-pages
+
+git clone https://github.com/zumwald/zumwald.github.io.git gh-pages
+
+cd $DIR
+pwd
+
+yarn fullbuild
+
+cd $DIR/../gh-pages
+git status
+git commit -a
+git push
+
+cd $DIR
