16d681abf028ffd8cc506aa658aea73461c555eb
Daniel Zumwalt <dazumwal@microsoft.com>
2019-09-10 21:04:59 -0700

    simplify typing

--- danzumwalt-com/themes/goa/static/js/typing.js
+++ danzumwalt-com/themes/goa/static/js/typing.js
@@ -24,19 +24,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
   // start a typewriter animation for a text in the dataText array
   function StartTextAnimation(i) {
-    if (typeof dataText[i] == "undefined") {
-      setTimeout(function() {
-        StartTextAnimation(0);
-      }, 5000);
-    }
-      
     // check if dataText[i] exists
-    if (i < dataText[i].length) {
+    if (i < dataText.length) {
       // text exists! start typewriter animation
       typeWriter(dataText[i], 0, function() {
         // after callback (and whole text has been animated), start next text
         StartTextAnimation(i + 1);
       });
+    } else {
+      setTimeout(function() {
+        StartTextAnimation(0);
+      }, 3000);
     }
   }
   // start the text animation
